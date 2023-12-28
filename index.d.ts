d37f13e79283a551e646929edee9674df833af74
BHARATH  M <150882708+bharath0spidey@users.noreply.github.com>
2023-12-28 15:08:47 +0530

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,5 @@
-export { default as useAutocomplete } from './useAutocomplete';
-export * from './useAutocomplete';
+export { default as parse } from './parse';
+export { default as stringify } from './stringify';
+export * from './type';
+export * from './CssParseError';
+export * from './CssPosition';
